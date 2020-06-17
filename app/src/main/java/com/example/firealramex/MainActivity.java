001b2b5de4d77031f75e3a1c5027bf91f75f8bd1
waterglasses <waterglasses_l@hanmail.net>
2020-06-17 20:26:16 +0900

    first

---
+++ app/src/main/java/com/example/firealramex/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.firealramex;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       // FirebaseMessaging.getInstance().subscribeToTopic("1");
+        FirebaseMessaging.getInstance().subscribeToTopic("Test");
+
+        Intent intent = getIntent();
+        if(intent != null) {
+            //푸시알림을 선택해서 실행한것이 아닌경우 예외처리
+            String notificationData = intent.getStringExtra("test");
+            if(notificationData != null)
+                Log.d("Firebase", notificationData);
+        }
+    }
+}
